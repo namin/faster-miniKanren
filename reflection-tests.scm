3fbadb74b2991980e32854c044f4a6931d6a9d3f
Nada Amin <namin@seas.harvard.edu>
2020-11-02 16:01:34 -0500

    some loops

--- reflection-tests.scm
+++ reflection-tests.scm
@@ -14,6 +14,23 @@
      (g)
      ((rfl-anyo g))))))
 
+(define rfl-loopanyo
+  (lambda (stamp g)
+    (fresh () (reflecto `(loopanyo ,stamp ,g))
+    (conde
+     (g)
+     ((fresh (stamp2)
+        (rfl-loopanyo stamp2 g)))))))
+
+(define rfl-loopanyo2
+  (lambda (stamp g)
+    (fresh () (reflecto `(loopanyo2 ,stamp ,g))
+    (conde
+     (g)
+     ((fresh (stamp2)
+        (== stamp stamp2)
+        (rfl-loopanyo2 stamp2 g)))))))
+
 (test "rfl-appendo-0"
       (run* (q) (rfl-appendo '(a b c) '(d e) q) inspecto)
       '((a b c d e)))
@@ -21,3 +38,11 @@
 (test "rfl-anyo-0"
       (run* (q) (rfl-anyo (== q '())) inspecto)
       '(()))
+
+(test "rfl-loopanyo-1"
+      (run 3 (q) (fresh (x) (rfl-loopanyo x (== q '())) inspecto))
+      '(() () ()))
+
+(test "rfl-loopanyo-2"
+      (run 3 (q) (fresh (x) (rfl-loopanyo2 x (== q '())) inspecto))
+      '(() () ()))
