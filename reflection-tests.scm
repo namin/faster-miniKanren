ccb7ae3000283fc6a8e23923c489a5263e8943af
Nada Amin <namin@seas.harvard.edu>
2020-11-02 15:35:54 -0500

    keep naming convention

--- reflection-tests.scm
+++ reflection-tests.scm
@@ -7,17 +7,17 @@
       (== zs (cons a r))
       (rfl-appendo d ys r))))))
 
-(define anyo
+(define rfl-anyo
   (lambda (g)
     (fresh () (reflecto `(anyo ,g))
     (conde
      (g)
-     ((anyo g))))))
+     ((rfl-anyo g))))))
 
 (test "rfl-appendo-0"
       (run* (q) (rfl-appendo '(a b c) '(d e) q) inspecto)
       '((a b c d e)))
 
 (test "rfl-anyo-0"
-      (run 1 (q) (anyo (== q '())) inspecto)
-      '())
+      (run* (q) (rfl-anyo (== q '())) inspecto)
+      '(()))
